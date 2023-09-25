d3e43677a39db8cc9aaf94a5e336783bc7e64e8c
vyolse <vyolse@gmail.com>
2023-09-25 11:08:24 +0300

    LIB-580 на проверку2

--- src/main/java/service/VerificationService.java
+++ src/main/java/service/VerificationService.java
@@ -1,16 +1,19 @@
 package service;
 
+import api.core.RequestParam;
+import io.restassured.http.Method;
 import io.restassured.response.Response;
 import pojo.userAccountService.GetVerificationCode;
 import pojo.userAccountService.PhoneVerificationRequest;
 import pojo.userAccountService.Verification;
+
 import static api.core.ApiClient.sendRequestWithoutParams;
 import static api.core.ApiClient.sendSimpleRequest;
+import static api.core.RequestParamType.PARAMETER;
 import static constant.ApiEndpoints.VERIFICATION_CODE;
 import static constant.ApiEndpoints.VERIFICATION;
-import static io.restassured.http.Method.GET;
-import static io.restassured.http.Method.POST;
-
+import static constant.UserServiceConstants.PARAMETER_RECEIVER;
+import static io.restassured.http.Method.*;
 
 
 public class VerificationService {
@@ -46,25 +49,15 @@ public class VerificationService {
     }
 
     public Response verificationService(String phoneNumber) {
-        List<RequestParam> params = Collections.singletonList(new RequestParam(PARAMETER, PARAMETER_RECEIVER, phoneNumber));
-        return sendSimpleRequest(PATCH, VERIFICATION_CODE, params);
+        return sendSimpleRequest(PATCH, VERIFICATION_CODE, new RequestParam(PARAMETER, PARAMETER_RECEIVER, phoneNumber));
     }
 
     public Response checkVerificationCodeWithoutParam() {
         return sendRequestWithoutParams(PATCH, VERIFICATION_CODE);
     }
 
-    public String userVerificationWithValidData(String mobilePhone) {
-        Response response = verificationService(mobilePhone);
-        String verificationCode = response.jsonPath().get("verificationCode");
-        return verificationCode;
-    }
-
     public Response checkVerificationCodeInvalidHttpMethod(String invalidHttpMethod, String phoneNumber) {
-        List<RequestParam> params = Collections.singletonList(new RequestParam(PARAMETER, PARAMETER_RECEIVER, phoneNumber));
-        return sendSimpleRequest(Method.valueOf(invalidHttpMethod), VERIFICATION_CODE, params);
+        return sendSimpleRequest(Method.valueOf(invalidHttpMethod), VERIFICATION_CODE, new RequestParam(PARAMETER, PARAMETER_RECEIVER, phoneNumber));
 
     }
-
 }
-
