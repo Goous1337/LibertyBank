2dd63e180c002abdbecc5872e882774f2766aea3
Denis Kovalev <kovalev-denis_1996@mail.ru>
2023-10-30 19:08:36 +0500

    IS-5

--- src/main/java/constant/LibertyServiceName.java
+++ src/main/java/constant/LibertyServiceName.java
@@ -8,7 +8,8 @@ import lombok.Getter;
 public enum LibertyServiceName {
     USER_SERVICE("user_service_db"),
     USER_ACCOUNT_SERVICE("user_account_service_db"),
-    CUSTOMER_SERVICE("customer_service_db");
+    CUSTOMER_SERVICE("customer_service_db"),
+    INFO_SERVICE("info_service_db");
 
     private final String serviceName;
 }
