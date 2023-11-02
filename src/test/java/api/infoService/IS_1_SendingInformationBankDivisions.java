11b28a3e6edf9d4f8440458ef47fcd69667de538
Sergei_Shabalin <sergeimkmouse@gmail.com>
2023-11-02 12:24:36 +0300

    Revert "IS_1"
    This reverts commit cc76a9916c117c5d2feb94ca8a023318f88751f9.

--- src/test/java/api/infoService/IS_1_SendingInformationBankDivisions.java
+++ src/test/java/api/infoService/IS_1_SendingInformationBankDivisions.java
@@ -57,10 +57,11 @@ public class IS_1_SendingInformationBankDivisions extends BaseTest {
                 () -> assertEquals(SC_OK, response.statusCode(),
                         "Код ответа не соответствует ожидаемому"),
                 () -> {
-                    List<String> items = response.jsonPath().getList("cityName");
-                    items.forEach(actualCityName -> assertEquals(expectedCityName, actualCityName,
-                            "Запрошенный город не соответствует представленному"));
-
+                    List<Map<String, Object>> items = response.jsonPath().getList("cityName");
+                    for (int i = 0; i < items.size(); i++) {
+                        assertEquals(expectedCityName, items.get(i),
+                                "Запрошенный город не соответствует представленному");
+                    }
                 },
                 () -> {
                     List<Map<String, Object>> items = response.jsonPath().getList("$");
