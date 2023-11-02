a2fd38daba4ba3b029ea4fb5514ee2ef8222fcbc
Siarhei Shabalin <s.shabalin@astondevs.ru>
2023-11-02 20:14:02 +0300

    LIB-971 Реализация InfoService IS-2

--- src/main/java/service/InfoService.java
+++ src/main/java/service/InfoService.java
@@ -9,15 +9,10 @@ import java.util.List;
 
 import static api.core.ApiClient.sendRequestWithoutParams;
 import static api.core.ApiClient.sendSimpleRequest;
-import static api.core.RequestParamType.HEADER;
 import static api.core.RequestParamType.PARAMETER;
-import static com.google.common.net.HttpHeaders.CONTENT_TYPE;
 import static constant.ApiEndpoints.*;
-import static constant.CustomerServiceConstants.PARAMETER_CUSTOMER_ID;
 import static constant.InfoServiceConstants.*;
-import static io.netty.handler.codec.http.HttpHeaders.Values.APPLICATION_JSON;
 import static io.restassured.http.Method.GET;
-import static io.restassured.http.Method.PATCH;
 
 public class InfoService {
     public Response successfulGettingCityList() {
@@ -52,6 +47,27 @@ public class InfoService {
 
     public Response checkGettingAllBankDivisionsInvalidMethod(String httpMethod) {
         return sendRequestWithoutParams(Method.valueOf(httpMethod), BANK_DIVISIONS_LIST);
+    }
 
+    public Response checkGettingInformationCurrencyExchangeRates() {
+        return sendRequestWithoutParams(GET, BANK_EXCHANGE_RATES);
+    }
+
+    public Response checkGettingInformationCurrencyExchangeRatesCityId(String cityId) {
+        return sendSimpleRequest(GET, BANK_EXCHANGE_RATES, new RequestParam(PARAMETER, PARAMETER_CITYID, cityId));
+    }
+
+    public Response checkGettingInformationCurrencyExchangeRateCityIdPageSize(String cityId, String pageNumb, String size) {
+        List<RequestParam> params = List.of(new RequestParam(PARAMETER, PARAMETER_CITYID, cityId),
+                new RequestParam(PARAMETER, PARAMETER_PAGENUMB, pageNumb), new RequestParam(PARAMETER, PARAMETER_PAGELIMIT, size));
+        return sendSimpleRequest(GET, BANK_EXCHANGE_RATES, params);
+    }
+
+    public Response checkGettingInformationCurrencyExchangeRatesInvalidUrl() {
+        return sendRequestWithoutParams(GET, INVALID_BANK_EXCHANGE_RATES);
+    }
+
+    public Response checkGettingInformationCurrencyExchangeRatesInvalidMethod(String httpMethod) {
+        return sendRequestWithoutParams(Method.valueOf(httpMethod), BANK_EXCHANGE_RATES);
     }
 }
