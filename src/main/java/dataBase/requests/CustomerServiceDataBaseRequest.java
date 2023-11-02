f1d49a1d8024e5d4499eb5749da044d376e5f471
Denis Kovalev <kovalev-denis_1996@mail.ru>
2023-11-02 15:48:53 +0500

    CRS 6 refactoring

--- src/main/java/dataBase/requests/CustomerServiceDataBaseRequest.java
+++ src/main/java/dataBase/requests/CustomerServiceDataBaseRequest.java
@@ -11,7 +11,7 @@ public class CustomerServiceDataBaseRequest {
     private static final Logger LOG = LogManager.getLogger("DBRequest");
 
     public static String receivingEmailCustomerByCustomerId(String customerId) {
-        String sql = "SELECT email FROM public.client WHERE customer_id=?::uuid";
+        String sql = "SELECT email FROM customer WHERE customer_id=?::uuid";
         String email = getDBConnection(CUSTOMER_SERVICE).queryForObject(sql, String.class, customerId);
         LOG.info(String.format("По id пользователя %s получен email %s", customerId, email));
         return email;
