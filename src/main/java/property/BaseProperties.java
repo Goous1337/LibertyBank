f356a9998ad84254c0c58b327262555465add284
quicksaver1 <quicksaver13@gmail.com>
2023-11-23 12:27:21 +0400

    cM 3 1

--- src/main/java/property/BaseProperties.java
+++ src/main/java/property/BaseProperties.java
@@ -8,5 +8,7 @@ public final class BaseProperties {
     public static String URL_USER_ACCOUNT_SERVICE = getPropertyValue("user_account_service");
     public static String CUSTOMER_SERVICE = getPropertyValue("customer_service");
     public static String INFO_SERVICE = getPropertyValue("info_service");
+    public static String CREDIT_SERVICE = getPropertyValue("credit_service");
+    public static String DEPOSIT_SERVICE = getPropertyValue("deposit_service");
 
 }
