26ab02543a4f17fd6c88c416a3b9423b5fe4e3d6
quicksaver1 <quicksaver13@gmail.com>
2023-11-23 12:38:18 +0400

    config fix for CM DM

--- src/main/java/property/BaseProperties.java
+++ src/main/java/property/BaseProperties.java
@@ -11,4 +11,6 @@ public final class BaseProperties {
     public static String CREDIT_SERVICE = getPropertyValue("credit_service");
     public static String DEPOSIT_SERVICE = getPropertyValue("deposit_service");
 
+    public static String ACCESS_TOKEN_CUSTOMER_SERVICE = getPropertyValue("access_token_credit_service");
+
 }
