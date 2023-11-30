6e35d7b055da9debb91aaa7ebb9de0170fef700e
VladislavMozheyko <mozheykovlad@mail.ru>
2023-11-30 11:28:38 +0300

    LIB3-536,LIB3-533,LIB3-537 - done

--- src/main/java/service/CreditService.java
+++ src/main/java/service/CreditService.java
@@ -12,8 +12,9 @@ import java.util.List;
 import static api.core.ApiClient.sendSimpleRequest;
 import static api.core.RequestParamType.HEADER;
 import static com.google.common.net.HttpHeaders.AUTHORIZATION;
-import static constant.ApiEndpoints.CREDIT_BODY;
+import static constant.ApiEndpoints.*;
 import static constant.DepositConstants.INVALID_ACCESS_TOKEN;
+import static io.restassured.http.Method.GET;
 import static io.restassured.http.Method.POST;
 import static property.BaseProperties.ACCESS_TOKEN_CUSTOMER_SERVICE;
 
@@ -27,6 +28,7 @@ public class CreditService {
                 (POST, CREDIT_BODY, params, new CreateApplyingLoanRequest(productId, amount, periodMonths,
                         currencyCode, creationDate, monthlyIncome, monthlyExpenditure, employerIdentificationNumber));
     }
+
     public Response checkListApplyingLoanInvalidToken
             (Integer productId, Integer amount, Integer periodMonths, String currencyCode, String creationDate,
              Integer monthlyIncome, Integer monthlyExpenditure, String employerIdentificationNumber) {
@@ -35,5 +37,19 @@ public class CreditService {
         return sendSimpleRequest
                 (POST, CREDIT_BODY, params, new CreateApplyingLoanRequest(productId, amount, periodMonths,
                         currencyCode, creationDate, monthlyIncome, monthlyExpenditure, employerIdentificationNumber));
+    }
+
+    public Response checkListNumberOfLoanApplicationsSubmitted() {
+        return sendSimpleRequest(GET, CREDIT_ORDER_STATUS,
+                new RequestParam(HEADER, AUTHORIZATION, ACCESS_TOKEN_CUSTOMER_SERVICE));
+    }
+
+    public Response checkListNumberOfLoanApplicationsSubmittedInvalidToken() {
+        return sendSimpleRequest(GET, CREDIT_ORDER_STATUS,
+                new RequestParam(HEADER, AUTHORIZATION, INVALID_ACCESS_TOKEN));
+    }
+    public Response checkListNumberOfLoanApplicationsSubmittedNoRecordsInTheTable() {
+        return sendSimpleRequest(GET, INVALID_CREDIT_ORDER_TABLE,
+                new RequestParam(HEADER, AUTHORIZATION, ACCESS_TOKEN_CUSTOMER_SERVICE));
     }
 }
