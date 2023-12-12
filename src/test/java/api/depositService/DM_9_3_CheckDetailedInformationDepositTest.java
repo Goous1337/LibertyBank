d14b403ff60723491b58420920d8ab25e910f948
Malgin Evgeny <eugenimalgin@yandex.ru>
2023-12-12 08:54:44 +0300

    add newTest

--- src/test/java/api/depositService/DM_9_3_CheckDetailedInformationDepositTest.java
+++ src/test/java/api/depositService/DM_9_3_CheckDetailedInformationDepositTest.java
@@ -9,11 +9,10 @@ import io.restassured.response.Response;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Tags;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
-import static org.apache.hc.core5.http.HttpStatus.SC_OK;
+import static org.apache.hc.core5.http.HttpStatus.*;
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static property.BaseProperties.DEPOSIT_SERVICE;
@@ -26,7 +25,7 @@ public class DM_9_3_CheckDetailedInformationDepositTest extends BaseTest {
 
     @DisplayName("Получение подробной информации о депозитном предложении")
     @Description("Данный тест-кейс направлен на проверку корректного получения подробной информации о выбранном депозитном предложении.")
-    @Tags({@Tag("API"),@Tag("Positive"), @Tag("backend")})
+    @Tags({@Tag("API"), @Tag("Positive"), @Tag("backend")})
     @TmsLink("https://jira.astondevs.ru/browse/LIB3-1099")
     @ParameterizedTest(name = "productId: {0}")
     @CsvSource({
@@ -37,7 +36,7 @@ public class DM_9_3_CheckDetailedInformationDepositTest extends BaseTest {
 
     public void checkDetailedInformationAboutDepositOffer(Integer productId) {
         Response response = depositService.checkDetailedInformationAboutDeposit(productId);
-        String jsonSchemaPath ="schemas/depositService/successfulGetDepositInfo.json";
+        String jsonSchemaPath = "schemas/depositService/successfulGetDepositInfo.json";
         assertAll(
                 () -> assertEquals(SC_OK,
                         response.statusCode(),
@@ -48,17 +47,21 @@ public class DM_9_3_CheckDetailedInformationDepositTest extends BaseTest {
 
     @DisplayName("Получение подробной информации о депозитном предложении при невалидных значениях")
     @Description("Данный тест-кейс направлен на проверку корректного получения подробной информации о выбранном депозитном предложении.")
-    @Tags({@Tag("API"),@Tag("Positive"), @Tag("backend")})
-    @TmsLink("https://jira.astondevs.ru/browse/LIB3-1099")
-    @Test
+    @Tags({@Tag("API"), @Tag("Negative"), @Tag("backend")})
+    @TmsLink("https://jira.astondevs.ru/browse/LIB3-282")
+    @ParameterizedTest(name = "productId: {0}")
+    @CsvSource({
+            "100",
+            "50",
+            "33"
+    })
 
-    public void DetailedInformationAboutDepositOffer(Integer productId) {
+    public void unsuccessfulDetailedInformationAboutDepositOffer(Integer productId) {
         Response response = depositService.checkDetailedInformationAboutDeposit(productId);
         assertAll(
-                () -> assertEquals(SC_OK,
+                () -> assertEquals(SC_NOT_FOUND,
                         response.statusCode(),
                         "Код ответа не соответствует ожидаемому")
         );
     }
-
 }
