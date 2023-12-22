01511cef7e2f5f90940970d6c0205eac3b72e648
Zankevich Maria <m.zankevich@astondevs.ru>
2023-12-22 12:46:24 +0300

    creat ABS_IS_4

--- src/main/java/constant/ApiEndpoints.java
+++ src/main/java/constant/ApiEndpoints.java
@@ -41,6 +41,7 @@ public final class ApiEndpoints {
     public static final String CREDIT_WITHDRAWAL = "credits/api/v1/credit-order/";
     public static final String NOT_EXIST_CREDIT_WITHDRAWAL = "credits/api/v1/credit-order/10000";
     public static final String CREDIT_INFO ="credits/api/v1/credit";
-
     public static final String CREDIT_INFORMATION = "credits/api/v1/credit/current";
+    public static final String CREDIT_SUM_CALCULATE = "credits/api/v1/credit-product/credit-calculate/sum-calculate";
+    public static final String BANK_LIST = "api/v1/info-service/offices";
 }
---
+++ src/test/java/api/absInfoServic/ABS_IS_4_CheckListOfBankBranchesAreGettingSuccessfullyTest.java
@@ -0,0 +1,44 @@
+package api.absInfoServic;
+
+import io.qameta.allure.Description;
+import io.qameta.allure.TmsLink;
+import io.restassured.RestAssured;
+import io.restassured.module.jsv.JsonSchemaValidator;
+import io.restassured.response.Response;
+import org.apache.http.HttpStatus;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Tags;
+import org.junit.jupiter.api.Test;
+import service.InfoService;
+
+import static org.asynchttpclient.util.HttpConstants.Methods.GET;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static property.BaseProperties.ABS_INFO_SERVICE;
+
+public class ABS_IS_4_CheckListOfBankBranchesAreGettingSuccessfullyTest {
+
+    {
+        RestAssured.baseURI = ABS_INFO_SERVICE;
+    }
+
+    @DisplayName("[IS-4] [STATUS CODE 200] (GET) Успешное получение списка филиалов")
+    @Description("Проверить, что приходит список всех филиалов банка")
+    @Tags({@Tag("API")})
+    @TmsLink("https://jira.astondevs.ru/browse/LIB4-932")
+    @Test
+
+    public void successfulGettingListOfBankBranches() {
+        String jsonSchemaPath = "schemas/IS_4_CheckListOfBanks/successfulGettingListOfBankBranches.json";
+        Response response = InfoService.gettingListOfBankBranches(GET);
+        assertAll(
+                () -> assertEquals(HttpStatus.SC_OK,
+                        response.statusCode(),
+                        "Код ответа не соответствует ожидаемому"),
+                () -> response.then().assertThat().body(JsonSchemaValidator.matchesJsonSchemaInClasspath(jsonSchemaPath))
+        );
+    }
+}
+
+
