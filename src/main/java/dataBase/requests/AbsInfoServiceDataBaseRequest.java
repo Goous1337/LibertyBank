9cf5e19d3c81ea9662dae14bf71f29388743a372
VladislavMozheyko <mozheykovlad@mail.ru>
2023-12-29 09:08:17 +0300

    fix sql request suntaxsis

--- src/main/java/dataBase/requests/AbsInfoServiceDataBaseRequest.java
+++ src/main/java/dataBase/requests/AbsInfoServiceDataBaseRequest.java
@@ -45,28 +45,31 @@ public class AbsInfoServiceDataBaseRequest {
 
     public static AbsInfoServiceDataBankBranch getBankBranchData(int id) {
 
-        String sqlBankOfficesData = "SELECT bank_office.office_number, bank_office.ramp,\n" +
-                "bank_office.phone_number,bank_office.is_closed,bank_office.opening_time,\n" +
-                "bank_office.closing_time,bank_office.currency_exchange,bank_office.exotic_currency,\n" +
-                "bank_office.money_transfer,bank_office.cash_withdrawal,bank_office.accept_payment,\n" +
-                "bank_office.replenish_card,bank_office.replenish_account,bank_office.consultation,\n" +
-                "bank_office.insurance,\n" +
-                "office_address.country, office_address.region,office_address.city,\n" +
-                "office_address.street,office_address.building_number,\n" +
-                "office_address.office_coordinates,\n" +
-                "office_address.post_code,branch_details.bik,branch_details.kpp,\n" +
-                "branch_details.inn,branch_details.okpo,branch_details.ogrn,\n" +
-                "branch_details.swift,\n" +
-                "branch_details.payment_account,branch_details.correspondent_account,\n" +
-                "branch_details.bank_name_full\n" +
-                "FROM bank_office \n" +
-                "INNER JOIN office_address \n" +
-                "ON bank_office.id = office_address.id \n" +
-                "INNER JOIN branch_details\n" +
-                "ON branch_details.id = bank_office.id\n" +
-                "AND address_id = ?";
-        AbsInfoServiceDataBankBranch absInfoServiceDataBankBranch = (AbsInfoServiceDataBankBranch) getDBConnection(ABS_INFO_SERVICE).queryForObject
-                (sqlBankOfficesData, new Object[]{id}, new BeanPropertyRowMapper(AbsInfoServiceDataBankBranch.class));
+        String sqlBankOfficesData = """
+                SELECT bank_office.office_number, bank_office.ramp,
+                bank_office.phone_number,bank_office.is_closed,bank_office.opening_time,
+                bank_office.closing_time,bank_office.currency_exchange,bank_office.exotic_currency,
+                bank_office.money_transfer,bank_office.cash_withdrawal,bank_office.accept_payment,
+                bank_office.replenish_card,bank_office.replenish_account,bank_office.consultation,
+                bank_office.insurance,
+                office_address.country, office_address.region,office_address.city,
+                office_address.street,office_address.building_number,
+                office_address.office_coordinates,
+                office_address.post_code,branch_details.bik,branch_details.kpp,
+                branch_details.inn,branch_details.okpo,branch_details.ogrn,
+                branch_details.swift,
+                branch_details.payment_account,branch_details.correspondent_account,
+                branch_details.bank_name_full
+                FROM bank_office
+                INNER JOIN office_address
+                ON bank_office.id = office_address.id
+                INNER JOIN branch_details
+                ON branch_details.id = bank_office.id
+                AND address_id = ?
+                """;
+        AbsInfoServiceDataBankBranch absInfoServiceDataBankBranch =
+                (AbsInfoServiceDataBankBranch) getDBConnection(ABS_INFO_SERVICE).queryForObject
+                        (sqlBankOfficesData, new Object[]{id}, new BeanPropertyRowMapper(AbsInfoServiceDataBankBranch.class));
         absInfoServiceDataBankBranch.setClosing_time(absInfoServiceDataBankBranch.getClosing_time().substring(0, 5));
         absInfoServiceDataBankBranch.setOpening_time(absInfoServiceDataBankBranch.getOpening_time().substring(0, 5));
         return absInfoServiceDataBankBranch;
