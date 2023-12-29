8ac818c379ee62fc801da2494a065190ada0a726
VladislavMozheyko <mozheykovlad@mail.ru>
2023-12-29 10:12:48 +0300

    Ð fix sql in DM 9.9

--- src/main/java/dataBase/requests/DepositServiceDataBaseRequest.java
+++ src/main/java/dataBase/requests/DepositServiceDataBaseRequest.java
@@ -10,8 +10,7 @@ public class DepositServiceDataBaseRequest {
     private static final Logger LOG = LogManager.getLogger("DBRequest");
 
     public static String getCapitalisationValue(int id, int termTime) {
-        String sql = "SELECT interest_rate FROM months_percent_borders WHERE deposit_product_id = " + id + "  AND " + termTime + " BETWEEN lower_months_border AND upper_months_border";
-        String value = getDBConnection(DEPOSIT_SERVICE_DB).queryForObject(sql, String.class);
-        return value;
+        String sql = "SELECT interest_rate FROM months_percent_borders WHERE deposit_product_id = %s AND %s BETWEEN lower_months_border AND upper_months_border";
+        return getDBConnection(DEPOSIT_SERVICE_DB).queryForObject(String.format(sql, id, termTime), String.class);
     }
 }
