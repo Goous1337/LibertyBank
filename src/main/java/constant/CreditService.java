757d77966ef22dd0109f6d54b8f11e403a505c8a
Zankevich Maria <m.zankevich@astondevs.ru>
2024-01-19 12:18:02 +0300

    create CRS_10

--- src/main/java/constant/CreditService.java
+++ src/main/java/constant/CreditService.java
@@ -8,20 +8,15 @@ public class CreditService {
     public static final String CURRENCY_CODE = "RUB";
     public static final int MIN_PERIOD_MONTHS = 10;
     public static final int MAX_PERIOD_MONTHS = 24;
-    public static final String NAME_INFO_CREDIT="Экспресс-кредит";
+    public static final String NAME_INFO_CREDIT = "Экспресс-кредит";
     public static final int CREDIT_LIMIT = 140000;
     public static final int PERIOD_MONTHS = 4;
     public static final int INTEREST_RATE = 22;
     public static final int GENERAL_DEPT = 0;
     public static final int CURR_MAIN_CREDIT_PAYMENT = 0;
     public static final int PERCENT_CREDIT_PAYMENT = 0;
-    public static final String PAYMENT_DATE="2023-10-25";
+    public static final String PAYMENT_DATE = "2023-10-25";
     public static final int OUTSTANDING_PRINCIPAL = 0;
-    public static final String NUMBER="123-OO";
-    public static final String CREDIT_ACCOUNT_NUMBER="0";
-
-
-
-
-
+    public static final String NUMBER = "123-OO";
+    public static final String CREDIT_ACCOUNT_NUMBER = "0";
 }
--- src/main/java/constant/LibertyServiceName.java
+++ src/main/java/constant/LibertyServiceName.java
@@ -13,6 +13,7 @@ public enum LibertyServiceName {
     ABS_INFO_SERVICE("abs-info_service_db"),
     DEPOSIT_SERVICE_DB("deposit_service_db"),
     СREDIT_SERVICE("credit_service_db"),
+    CUSTOMER_SERVICE_2_0("customer2_service_db"),
     ABS_CLIENT_SERVICE_BD("abs_client_service_db");
 
     private final String serviceName;
