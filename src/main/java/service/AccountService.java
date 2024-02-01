325986a8969b63ffc9098947c6dbea99dbd452d6
Daria Musaeva <d.musaeva@astondevs.ru>
2024-02-01 08:34:18 +0000

    added tests for create account

--- src/main/java/service/AccountService.java
+++ src/main/java/service/AccountService.java
@@ -1,15 +1,15 @@
 package service;
 
 import io.restassured.response.Response;
+import pojo.accountService.AccountData;
 import pojo.accountService.ChangeAccountStatusRequest;
-
 import static api.core.ApiClient.sendSimpleRequest;
 import static api.core.RequestParam.getRP;
 import static api.core.RequestParamType.HEADER;
-import static constant.AccountServiceConstants.HEADER_CUSTOMER_ID;
-import static constant.AccountServiceConstants.VALID_CUSTOMER_ID;
+import static constant.AccountServiceConstants.*;
 import static constant.ApiEndpoints.ACCOUNTS_LIST;
 import static io.restassured.http.Method.GET;
+import static io.restassured.http.Method.POST;
 import static io.restassured.http.Method.PATCH;
 import static property.BaseProperties.CUSTOMER_ID;
 
@@ -19,6 +19,17 @@ public class AccountService {
         return sendSimpleRequest(GET, ACCOUNTS_LIST,
                     getRP(HEADER, HEADER_CUSTOMER_ID, VALID_CUSTOMER_ID));
     }
+
+    public Response checkCreateNewAccount(String currency, String accountType, Boolean isMain) {
+        return sendSimpleRequest(POST, ACCOUNTS_LIST,
+                getRP(HEADER, HEADER_CUSTOMER_ID, VALID_CUSTOMER_ID),
+                new AccountData(currency, accountType, isMain));
+    }
+
+    public Response checkCreateNewAccountWithInvalidUserData(String currency, String accountType, Boolean isMain) {
+        return sendSimpleRequest(POST, ACCOUNTS_LIST,
+                getRP(HEADER, HEADER_CUSTOMER_ID, INVALID_CUSTOMER_ID),
+                new AccountData(currency, accountType, isMain));
 
     public Response changeAccountStatus(String accountId, String status) {
         return sendSimpleRequest(PATCH, ACCOUNTS_LIST + "/" + accountId,
