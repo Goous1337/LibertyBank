f485e6efabf20ddd138ef155f5c0b5b8b0f3cb83
Luiza Khaibullina <l.khaibullina@astondevs.ru>
2024-02-01 10:42:12 +0000

    as-3 new test

--- src/main/java/constant/AccountServiceConstants.java
+++ src/main/java/constant/AccountServiceConstants.java
@@ -2,6 +2,7 @@ package constant;
 
 public class AccountServiceConstants {
     public static final String VALID_CUSTOMER_ID = "c1c12bfb-ee07-42ee-9171-b705cebd6309";
+    public static final String CUSTOMER_WITH_NO_ACCOUNTS = "8e449009-e53b-453f-97d6-8d53524e75f9";
     public static final String INVALID_CUSTOMER_ID ="00000000-0000-0000-0000-00000000000";
     public static final String HEADER_CUSTOMER_ID = "X-Customer-Id";
     public static final String CURRENCY_RUB = "RUB";
