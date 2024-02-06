94966513d571729f74bed386412b210ba0aa6a49
Malgin Evgeny <eugenimalgin@yandex.ru>
2024-02-06 16:42:55 +0300

    test CRS-5 disabled

--- src/test/java/api/customerService_2_0/CRS_5_SettingNewPasswordTest.java
+++ src/test/java/api/customerService_2_0/CRS_5_SettingNewPasswordTest.java
@@ -8,10 +8,7 @@ import io.restassured.RestAssured;
 import io.restassured.module.jsv.JsonSchemaValidator;
 import io.restassured.response.Response;
 import org.apache.hc.core5.http.HttpStatus;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Tag;
-import org.junit.jupiter.api.Tags;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import pojo.customerService_2_0.CustomerService_2_0_Mobile;
@@ -21,14 +18,14 @@ import static org.apache.hc.core5.http.HttpStatus.*;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static property.BaseProperties.CUSTOMER_SERVICE_2_0;
-
+@Disabled("Жду создания тестовых пользователей")
 @DisplayName("CRS-5 Установление нового пароля")
 public class CRS_5_SettingNewPasswordTest extends BaseTest {
 
     {
         RestAssured.baseURI = CUSTOMER_SERVICE_2_0;
     }
-
+    @Disabled
     @DisplayName("Основной сценарий. Установка нового пароля")
     @Description("Данный тест-кейс проверяет успешную установку нового пароля пользователю.")
     @Tags({@Tag("API"), @Tag("Smoke"), @Tag("Positive")})
@@ -54,7 +51,7 @@ public class CRS_5_SettingNewPasswordTest extends BaseTest {
         );
         CustomerService_2_0_DataBaseRequest.resetTimerOfVerificationCodeById(customerId);
     }
-
+    @Disabled
     @DisplayName("Валидация метода запроса установления пароля")
     @Description("Данный тест-кейс проверяет возможность отправки запроса методом отличным от PATCH.")
     @Tags({@Tag("API"), @Tag("Negative")})
@@ -85,7 +82,7 @@ public class CRS_5_SettingNewPasswordTest extends BaseTest {
         );
         CustomerService_2_0_DataBaseRequest.resetTimerOfVerificationCodeById(customerId);
     }
-
+    @Disabled
     @DisplayName("Проверка обязательности значения token в теле запроса при установлении нового пароля")
     @Description("Данный тест-кейс проверяет обязательность token в теле запроса")
     @Tags({@Tag("API"), @Tag("Negative")})
@@ -103,7 +100,7 @@ public class CRS_5_SettingNewPasswordTest extends BaseTest {
                 () -> assertNotNull(response.getBody(), "Сообщение об ошибке отсутсвует")
         );
     }
-
+    @Disabled
     @DisplayName("Проверка обязательности значения password в теле запроса при установлении нового пароля")
     @Description("Данный тест-кейс проверяет обязательность password в теле запроса")
     @Tags({@Tag("API"), @Tag("Negative")})
@@ -129,7 +126,7 @@ public class CRS_5_SettingNewPasswordTest extends BaseTest {
         );
         CustomerService_2_0_DataBaseRequest.resetTimerOfVerificationCodeById(customerId);
     }
-
+    @Disabled
     @DisplayName("Проверка обязательности значения password в теле запроса при установлении нового пароля")
     @Description("Данный тест-кейс проверяет обязательность password в теле запроса")
     @Tags({@Tag("API"), @Tag("Negative")})
@@ -154,7 +151,7 @@ public class CRS_5_SettingNewPasswordTest extends BaseTest {
         );
         CustomerService_2_0_DataBaseRequest.resetTimerOfVerificationCodeById(customerId);
     }
-
+    @Disabled
     @DisplayName("Проверка обязательности значения password в теле запроса при установлении нового пароля")
     @Description("Данный тест-кейс проверяет обязательность password в теле запроса")
     @Tags({@Tag("API"), @Tag("Negative")})
@@ -173,7 +170,7 @@ public class CRS_5_SettingNewPasswordTest extends BaseTest {
                 () -> assertNotNull(response.getBody(), "Сообщение об ошибке отсутсвует")
         );
     }
-
+    @Disabled
     @DisplayName("Валидация входных параметров при установлении нового пароля")
     @Description("Данный тест-кейс проверяет валидацию входных параметров: newPassword.")
     @Tags({@Tag("API"), @Tag("Negative")})
