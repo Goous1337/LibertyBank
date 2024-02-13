68dd9fdcb8b2f5da1fe92f52ff9ebd7e8dcbbf57
dral0w <dralow221@gmail.com>
2024-02-13 14:55:44 +0300

    Добавлен тест на просмотр карточных продуктов

---
+++ src/test/java/api/cardService/CS_1_CheckAllCardProductsTest.java
@@ -0,0 +1,37 @@
+package api.cardService;
+
+import api.BaseTest;
+import io.qameta.allure.Description;
+import io.qameta.allure.TmsLink;
+import io.restassured.RestAssured;
+import io.restassured.module.jsv.JsonSchemaValidator;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import static org.apache.hc.core5.http.HttpStatus.SC_OK;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static property.BaseProperties.CARD_SERVICE;
+
+public class CS_1_CheckAllCardProductsTest extends BaseTest {
+    public static final String JSON_SCHEMA = "schemas/cardService/checkAllCardProducts.json";
+
+    {
+        RestAssured.baseURI = CARD_SERVICE;
+    }
+
+    @Test
+    @Tag("API")
+    @TmsLink("LIB2-2769")
+    @DisplayName("Просмотр карточных продуктов")
+    @Description("Тест направлен на проверку просмотра карточных продуктов")
+    public void checkAll() {
+        Response response = cardService.getCardsInfo();
+        assertAll(
+                () -> assertEquals(SC_OK, response.statusCode(), "Код ответа не соответствует ожидаемому"),
+                () -> response.then().assertThat().body(JsonSchemaValidator.matchesJsonSchemaInClasspath(JSON_SCHEMA))
+        );
+    }
+}
