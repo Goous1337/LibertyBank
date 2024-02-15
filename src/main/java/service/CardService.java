1b0b88020bc1c1fb44d2d69d04b68ab816e5d904
dral0w <dralow221@gmail.com>
2024-02-15 14:48:38 +0300

    Добавлен тест CS-3

--- src/main/java/service/CardService.java
+++ src/main/java/service/CardService.java
@@ -7,11 +7,17 @@ import static api.core.RequestParam.getRP;
 import static api.core.RequestParamType.*;
 import static constant.AccountServiceConstants.*;
 import static constant.ApiEndpoints.*;
+import static constant.CardServiceConstants.*;
 import static io.restassured.http.Method.GET;
 
 public class CardService {
     public Response getCardsInfo() {
         return sendSimpleRequest(GET, CARDS_LIST,
                 getRP(HEADER, HEADER_CUSTOMER_ID, VALID_CUSTOMER_ID));
+    }
+
+    public Response getActiveCardsInfoUserHaveCards() {
+        return  sendSimpleRequest(GET, ACTIVE_CARDS,
+                getRP(QUERY_PARAMETER, PARAMETER_CUSTOMER_ID, CUSTOMER_ID_WITH_ACTIVE_CARDS));
     }
 }
--- src/test/java/api/cardService/CS_3_CheckActiveCardsTest.java
+++ src/test/java/api/cardService/CS_3_CheckActiveCardsTest.java
@@ -10,13 +10,14 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
+import static org.apache.hc.core5.http.HttpStatus.SC_NOT_FOUND;
 import static org.apache.hc.core5.http.HttpStatus.SC_OK;
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static property.BaseProperties.CARD_SERVICE;
 
-public class CS_1_CheckAllCardProductsTest extends BaseTest {
-    public static final String JSON_SCHEMA = "schemas/cardService/checkAllCardProducts.json";
+public class CS_3_CheckActiveCardsTest extends BaseTest {
+    public static String JSON_SCHEMA_WITH_CARDS = "schemas/cardService/checkActiveCards.json";
 
     {
         RestAssured.baseURI = CARD_SERVICE;
@@ -24,14 +25,14 @@ public class CS_1_CheckAllCardProductsTest extends BaseTest {
 
     @Test
     @Tag("API")
-    @TmsLink("LIB2-2769")
-    @DisplayName("Просмотр карточных продуктов")
-    @Description("Тест направлен на проверку просмотра карточных продуктов")
-    public void checkAllCardProducts() {
-        Response response = cardService.getCardsInfo();
+    @TmsLink("LIB2-40")
+    @DisplayName("Просмотр активных карт")
+    @Description("Тест направлен на проверку возможности просмотра активных карт")
+    public void checkActiveCardsWithActiveCards() {
+        Response response = cardService.getActiveCardsInfoUserHaveCards();
         assertAll(
                 () -> assertEquals(SC_OK, response.statusCode(), "Код ответа не соответствует ожидаемому"),
-                () -> response.then().assertThat().body(JsonSchemaValidator.matchesJsonSchemaInClasspath(JSON_SCHEMA))
+                () -> response.then().assertThat().body(JsonSchemaValidator.matchesJsonSchemaInClasspath(JSON_SCHEMA_WITH_CARDS))
         );
     }
 }
