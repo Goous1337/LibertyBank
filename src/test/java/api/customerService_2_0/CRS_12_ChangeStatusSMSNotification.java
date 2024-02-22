6b077345d6253f2f945142cdb3cce8144340f333
quicksaver1 <quicksaver13@gmail.com>
2024-02-22 13:20:47 +0400

    reformat crs 12

--- src/test/java/api/customerService_2_0/CRS_12_ChangeStatusSMSNotification.java
+++ src/test/java/api/customerService_2_0/CRS_12_ChangeStatusSMSNotification.java
@@ -30,115 +30,70 @@ public class CRS_12_ChangeStatusSMSNotification extends BaseTest {
     }
 
     @DisplayName("Основной сценарий. Изменение статуса настройки получения SMS-уведомлений")
-    @Description("Данный тест-кейс позволяет вносить изменения в учетной записи Пользователя в части настроек" +
-            " получения SMS-уведомлений notificationStatus. В настройках пользователь может активировать или " +
-            "деактивировать чек-бокс на получение SMS-уведомлений. ")
+    @Description("Данный тест-кейс позволяет вносить изменения в учетной записи Пользователя в части настроек" + " получения SMS-уведомлений notificationStatus. В настройках пользователь может активировать или " + "деактивировать чек-бокс на получение SMS-уведомлений. ")
     @Tags({@Tag("API"), @Tag("N-F"), @Tag("Positive"), @Tag("Smoke")})
     @TmsLink("LIB-2758")
     @Test
     public void successChangeStatusSMSNotification() {
-        Response authResponse = customerService_2_0.userAuthorizationByMobilePhone(new UserAuthorizationByPhone
-                (CUSTOMER_USER_PHONE, CUSTOMER_USER_PASSWORD, CUSTOMER_MOBILE_PHONE_TYPE));
+        Response authResponse = customerService_2_0.userAuthorizationByMobilePhone(new UserAuthorizationByPhone(CUSTOMER_USER_PHONE, CUSTOMER_USER_PASSWORD, CUSTOMER_MOBILE_PHONE_TYPE));
         String token = authResponse.jsonPath().get("accessToken");
         boolean status = CustomerService_2_0_DataBaseRequest.getSMSStatusFromMobile(CUSTOMER_USER_PHONE);
 
         Response response = customerService_2_0.changeStatusSMSNotification(token, new NotificationStatus(!status));
-        assertAll(
-                () -> assertEquals(SC_OK, response.getStatusCode(), RESPONSE_CODE_NOT_EXPECTED),
-                () -> assertEquals(
-                        !status,
-                        CustomerService_2_0_DataBaseRequest.getSMSStatusFromMobile(CUSTOMER_USER_PHONE),
-                        "NotificationStatus не изменился")
-        );
+        assertAll(() -> assertEquals(SC_OK, response.getStatusCode(), RESPONSE_CODE_NOT_EXPECTED), () -> assertEquals(!status, CustomerService_2_0_DataBaseRequest.getSMSStatusFromMobile(CUSTOMER_USER_PHONE), "NotificationStatus не изменился"));
     }
 
     @DisplayName("Проверка работы системы при отсутствии данных в BODY")
-    @Description("Данном тест-кейсе проводится проверка работы сервиса изменения статуса настройки получения " +
-            "SMS-уведомлений, если передано пустое поле")
+    @Description("Данном тест-кейсе проводится проверка работы сервиса изменения статуса настройки получения " + "SMS-уведомлений, если передано пустое поле")
     @Tags({@Tag("API"), @Tag("N-F"), @Tag("Negative")})
     @TmsLink("LIB-2766")
     @Test
     public void unsuccessfulChangeStatusSMSNotificationWithEmptyBody() {
-        Response authResponse = customerService_2_0.userAuthorizationByMobilePhone(new UserAuthorizationByPhone
-                (CUSTOMER_USER_PHONE, CUSTOMER_USER_PASSWORD, CUSTOMER_MOBILE_PHONE_TYPE));
+        Response authResponse = customerService_2_0.userAuthorizationByMobilePhone(new UserAuthorizationByPhone(CUSTOMER_USER_PHONE, CUSTOMER_USER_PASSWORD, CUSTOMER_MOBILE_PHONE_TYPE));
         String token = authResponse.jsonPath().get("accessToken");
         Response response = customerService_2_0.changeStatusSMSNotification(token, new NotificationStatus(null));
-        assertAll(
-                () -> assertEquals(SC_BAD_REQUEST, response.getStatusCode(), RESPONSE_CODE_NOT_EXPECTED),
-                () -> assertEquals(
-                        "Некорректный запрос. Убедитесь, что адрес указан верно и попробуйте еще раз.",
-                        response.body().jsonPath().get("message"),
-                        ERROR_MESSAGE_NOT_EXPECTED)
-        );
+        assertAll(() -> assertEquals(SC_BAD_REQUEST, response.getStatusCode(), RESPONSE_CODE_NOT_EXPECTED), () -> assertEquals("Некорректный запрос. Убедитесь, что адрес указан верно и попробуйте еще раз.", response.body().jsonPath().get("message"), ERROR_MESSAGE_NOT_EXPECTED));
     }
 
     @DisplayName("Проверка работы системы при использовании некорректного URL")
-    @Description("Данном тест-кейсе проводится проверка работы сервиса изменения статуса настройки получения " +
-            "SMS-уведомлений при указании некорректного URL ")
+    @Description("Данном тест-кейсе проводится проверка работы сервиса изменения статуса настройки получения " + "SMS-уведомлений при указании некорректного URL ")
     @Tags({@Tag("API"), @Tag("N-F"), @Tag("Negative")})
     @TmsLink("LIB-2767")
     @Test
     public void unsuccessfulChangeStatusSMSNotificationWithIncorrectURL() {
-        Response authResponse = customerService_2_0.userAuthorizationByMobilePhone(new UserAuthorizationByPhone
-                (CUSTOMER_USER_PHONE, CUSTOMER_USER_PASSWORD, CUSTOMER_MOBILE_PHONE_TYPE));
+        Response authResponse = customerService_2_0.userAuthorizationByMobilePhone(new UserAuthorizationByPhone(CUSTOMER_USER_PHONE, CUSTOMER_USER_PASSWORD, CUSTOMER_MOBILE_PHONE_TYPE));
         String token = authResponse.jsonPath().get("accessToken");
-        Response response = customerService_2_0.changeStatusSMSNotificationWithIncorrectURL(
-                token, new NotificationStatus(false));
-        assertAll(
-                () -> assertEquals(SC_NOT_FOUND, response.getStatusCode(), RESPONSE_CODE_NOT_EXPECTED),
-                () -> assertEquals("Страница не найдена",
-                        response.body().jsonPath().get("message"),
-                        ERROR_MESSAGE_NOT_EXPECTED)
-        );
+        Response response = customerService_2_0.changeStatusSMSNotificationWithIncorrectURL(token, new NotificationStatus(false));
+        assertAll(() -> assertEquals(SC_NOT_FOUND, response.getStatusCode(), RESPONSE_CODE_NOT_EXPECTED), () -> assertEquals("Страница не найдена", response.body().jsonPath().get("message"), ERROR_MESSAGE_NOT_EXPECTED));
     }
 
     @DisplayName("Проверка работы системы при использовании метода запроса не PATCH/OPTIONS")
-    @Description("Данном тест-кейсе проводится проверка возможности отправки другим методом помимо PATCH/OPTIONS, " +
-            "которые являются разрешенными")
+    @Description("Данном тест-кейсе проводится проверка возможности отправки другим методом помимо PATCH/OPTIONS, " + "которые являются разрешенными")
     @Tags({@Tag("API"), @Tag("N-F"), @Tag("Negative")})
     @TmsLink("LIB-2768")
     @ParameterizedTest
     @ValueSource(strings = {"GET", "PUT", "DELETE", "POST"})
     public void unsuccessfulChangeStatusSMSNotificationWithInvalidMethods(String method) {
-        Response authResponse = customerService_2_0.userAuthorizationByMobilePhone(new UserAuthorizationByPhone
-                (CUSTOMER_USER_PHONE, CUSTOMER_USER_PASSWORD, CUSTOMER_MOBILE_PHONE_TYPE));
+        Response authResponse = customerService_2_0.userAuthorizationByMobilePhone(new UserAuthorizationByPhone(CUSTOMER_USER_PHONE, CUSTOMER_USER_PASSWORD, CUSTOMER_MOBILE_PHONE_TYPE));
         String token = authResponse.jsonPath().get("accessToken");
-        Response response = customerService_2_0.changeStatusSMSNotificationWithHttpMethod(
-                token, method, new NotificationStatus(false));
-        assertAll(
-                () -> assertEquals(SC_METHOD_NOT_ALLOWED, response.getStatusCode(), RESPONSE_CODE_NOT_EXPECTED),
-                () -> assertEquals("Метод не разрешен. Сервер знает о запрашиваемом методе, но он был деактивирован и не может быть использован.",
-                        response.body().jsonPath().get("message"),
-                        ERROR_MESSAGE_NOT_EXPECTED)
-        );
+        Response response = customerService_2_0.changeStatusSMSNotificationWithHttpMethod(token, method, new NotificationStatus(false));
+        assertAll(() -> assertEquals(SC_METHOD_NOT_ALLOWED, response.getStatusCode(), RESPONSE_CODE_NOT_EXPECTED), () -> assertEquals("Метод не разрешен. Сервер знает о запрашиваемом методе, но он был деактивирован и не может быть использован.", response.body().jsonPath().get("message"), ERROR_MESSAGE_NOT_EXPECTED));
     }
 
     static Stream<Object> provideArgumentsForTest() {
-        return Stream.of(
-                "",
-                123,
-                "hello"
-        );
+        return Stream.of("", 123, "hello");
     }
 
     @DisplayName("Проверка работы системы при передаче неправильного типа параметра notificationStatus")
-    @Description("Данном тест-кейсе проводится проверка работы сервиса при передаче неправильного типа параметра " +
-            "notificationStatus")
+    @Description("Данном тест-кейсе проводится проверка работы сервиса при передаче неправильного типа параметра " + "notificationStatus")
     @Tags({@Tag("API"), @Tag("N-F"), @Tag("Negative")})
     @TmsLink("LIB-2769")
     @ParameterizedTest
     @MethodSource("provideArgumentsForTest")
     public void unsuccessfulChangeStatusSMSNotificationWithInvalidParameters(Object value) {
-        Response authResponse = customerService_2_0.userAuthorizationByMobilePhone(new UserAuthorizationByPhone
-                (CUSTOMER_USER_PHONE, CUSTOMER_USER_PASSWORD, CUSTOMER_MOBILE_PHONE_TYPE));
+        Response authResponse = customerService_2_0.userAuthorizationByMobilePhone(new UserAuthorizationByPhone(CUSTOMER_USER_PHONE, CUSTOMER_USER_PASSWORD, CUSTOMER_MOBILE_PHONE_TYPE));
         String token = authResponse.jsonPath().get("accessToken");
-        Response response = customerService_2_0.changeStatusSMSNotificationWithValue(
-                token, value);
-        assertAll(
-                () -> assertEquals(SC_UNSUPPORTED_MEDIA_TYPE, response.getStatusCode(), RESPONSE_CODE_NOT_EXPECTED),
-                () -> assertEquals("Формат запрашиваемых данных не поддерживается сервером, поэтому запрос отклонён.",
-                        response.body().jsonPath().get("message"),
-                        ERROR_MESSAGE_NOT_EXPECTED)
-        );
+        Response response = customerService_2_0.changeStatusSMSNotificationWithValue(token, value);
+        assertAll(() -> assertEquals(SC_UNSUPPORTED_MEDIA_TYPE, response.getStatusCode(), RESPONSE_CODE_NOT_EXPECTED), () -> assertEquals("Формат запрашиваемых данных не поддерживается сервером, поэтому запрос отклонён.", response.body().jsonPath().get("message"), ERROR_MESSAGE_NOT_EXPECTED));
     }
 }
