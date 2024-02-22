9fc868d16075baebcbab311905de36e6a0aa9edc
quicksaver1 <quicksaver13@gmail.com>
2024-02-22 13:19:31 +0400

    white spaces fix

--- src/main/java/dataBase/requests/CustomerService_2_0_DataBaseRequest.java
+++ src/main/java/dataBase/requests/CustomerService_2_0_DataBaseRequest.java
@@ -157,7 +157,7 @@ public class CustomerService_2_0_DataBaseRequest {
     }
 
     public static Boolean getSMSStatusFromMobile(String mobile) {
-        String sql = "SELECT sms_notification FROM customer WHERE mobile_phone = '"+ mobile + "'";
+        String sql = "SELECT sms_notification FROM customer WHERE mobile_phone = '" + mobile + "'";
         return getDBConnection(CUSTOMER_SERVICE_DB_2_0).queryForObject(sql, Boolean.class);
     }
 
