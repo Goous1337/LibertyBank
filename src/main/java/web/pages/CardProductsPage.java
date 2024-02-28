7de344df3b68e41b213ea9e8d9088b9fbffbd957
dral0w <dralow221@gmail.com>
2024-02-28 17:34:51 +0300

    правки

--- src/main/java/web/pages/CardProductsPage.java
+++ src/main/java/web/pages/CardProductsPage.java
@@ -1,7 +1,6 @@
 package web.pages;
 
 import org.openqa.selenium.NoSuchElementException;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
