f3a7e4fc7fb1b0d56bbe099537bb50b22c6e47ae
Vlad-Mezhevich <anarezpb@gmail.com>
2024-02-28 13:58:36 +0300

    Исправление тестов CM 3.6. Удаление теста на вывод информации по кредитным продуктам с невалидным токеном.

--- src/main/java/service/CreditService.java
+++ src/main/java/service/CreditService.java
@@ -115,11 +115,6 @@ public class CreditService {
                 new RequestParam(HEADER, AUTHORIZATION, ACCESS_TOKEN_CUSTOMER_SERVICE));
     }
 
-    public Response checkListObtainingInformationOnBanksLoanProductInvalidToken() {
-        return sendSimpleRequest(GET, CREDIT_PRODUCTS_INFO,
-                new RequestParam(HEADER, AUTHORIZATION, INVALID_TOKEN_CREDIT_SERVICE));
-    }
-
     public Response checkListObtainingInformationOnBanksLoanProductNoRecordsInResultingTable() {
         return sendSimpleRequest(GET, INVALID_CREDIT_PRODUCT,
                 new RequestParam(HEADER, AUTHORIZATION, ACCESS_TOKEN_CUSTOMER_SERVICE));
