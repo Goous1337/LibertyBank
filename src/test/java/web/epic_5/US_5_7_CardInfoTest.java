9d7f59edc05c2fce987523c5119c3b60ef8dc86c
dral0w <dralow221@gmail.com>
2024-02-29 16:33:42 +0300

    Добавил константы в параметры теста

--- src/test/java/web/epic_5/US_5_7_CardInfoTest.java
+++ src/test/java/web/epic_5/US_5_7_CardInfoTest.java
@@ -9,6 +9,8 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import web.BaseTest;
 
+import static constant.CardInfoConstants.*;
+import static constant.CardServiceConstants.*;
 import static web.constans.UrlConfig.*;
 
 public class US_5_7_CardInfoTest extends BaseTest {
@@ -23,13 +25,13 @@ public class US_5_7_CardInfoTest extends BaseTest {
     @DisplayName("Проверка отображения необходимой информации о карте")
     @Description("Должны корректно отображаться имя, срок действия, стоимость обслуживания и валюта карты")
     @CsvSource({
-            "Liberty Card Classic, 60 месяцев, 99 ₽, RUB",
-            "Liberty Card Child, 36 месяцев, 0 ₽, RUB",
-            "Liberty Card Gold, 60 месяцев, 500 ₽ \\ 5 $ \\ 5 €, RUB \\ USD \\ EUR",
-            "Liberty Card Platinum, 60 месяцев, 10 $ \\ 1 000 ₽ \\ 10 €, USD \\ RUB \\ EUR",
-            "Liberty Card Virtual, 36 месяцев, 0 ₽ \\ 0 $ \\ 0 €, RUB \\ USD \\ EUR",
-            "Liberty Card Secure, 12 месяцев, 0 ₽ \\ 0 $ \\ 0 €, RUB \\ USD \\ EUR",
-            "Liberty Card Travel, 60 месяцев, 20 $ \\ 20 €, USD \\ EUR"
+            CARD_NAME_CLASSIC + ", " + VALIDITY_60_MONTHS + ", " + CLASSIC_CARD_COST + ", " + CURRENCY_RUB,
+            CARD_NAME_CHILD + ", " + VALIDITY_36_MONTHS + ", " + CHILD_CARD_COST + ", " + CURRENCY_RUB,
+            CARD_NAME_GOLD + ", " + VALIDITY_60_MONTHS + ", " + GOLD_CARD_COST + ", " + CURRENCY_RUB_USD_EUR,
+            CARD_NAME_PLATINUM + ", " + VALIDITY_60_MONTHS + ", " + PLATINUM_CARD_COST + ", " + CURRENCY_USD_RUB_EUR,
+            CARD_NAME_VIRTUAL + ", " + VALIDITY_36_MONTHS + ", " + VIRTUAL_CARD_COST + ", " + CURRENCY_RUB_USD_EUR,
+            CARD_NAME_SECURE + ", " + VALIDITY_12_MONTHS + ", " + SECURE_CARD_COST + ", " + CURRENCY_RUB_USD_EUR,
+            CARD_NAME_TRAVEL + ", " + VALIDITY_60_MONTHS + ", " + TRAVEL_CARD_COST + ", " + CURRENCY_USD_EUR
     })
     public void cardProductsInfoTest(String name, String validity, String cost, String currency) {
         open(CARD_PRODUCTS_URL + "/" + name);
