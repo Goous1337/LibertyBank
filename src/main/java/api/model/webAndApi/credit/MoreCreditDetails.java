7aca50cb8fc6b9083dd7ef12824b2193cc8ef826
Vlad-Mezhevich <anarezpb@gmail.com>
2024-03-26 10:45:37 +0300

    Поправка в тестах US 3.6

--- src/main/java/api/model/webAndApi/credit/MoreCreditDetails.java
+++ src/main/java/api/model/webAndApi/credit/MoreCreditDetails.java
@@ -26,4 +26,17 @@ public class MoreCreditDetails {
         this.details = details;
     }
 
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (!(obj instanceof MoreCreditDetails)) {
+            return false;
+        }
+        MoreCreditDetails moreCreditDetails = (MoreCreditDetails) obj;
+        return getHeader().equals(moreCreditDetails.getHeader());
+    }
+
 }
