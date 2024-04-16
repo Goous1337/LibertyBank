1a4ddf8d5da9c7efead5a2a701fcc45f7457fb78
Alexandr Sukhover <aleksandr.sukhover@gmail.com>
2024-04-16 10:43:58 +0300

    3

--- src/main/java/constant/InsuranceServiceConstants.java
+++ src/main/java/constant/InsuranceServiceConstants.java
@@ -4,6 +4,10 @@ public class InsuranceServiceConstants {
 
     public static final String CLIENT_ID = "813f5509-7696-44be-a321-3a094c48a6e7";
 
+    public static final String POLICY_THING_ID = "7223a5c7-ac5d-46c1-bcd7-063908674755";
+
+    public static final String POLICY_VEHICLE_ID = "b28ca4de-09f3-11ee-be56-0242ac120002";
+
     public static final String CONTENT_TYPE_VALUE = "application/json";
 
     public static final String INSURANCE_TYPE_VALUE = "VEHICLE";
