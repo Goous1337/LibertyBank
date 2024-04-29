0328b8dba54ad08a8c6317e9df970004188bd5ec
Hleb Zhuk <hleb7199@gmail.com>
2024-04-29 17:04:12 +0300

    Correct pipeline2

--- src/main/java/constant/InsuranceServiceConstants.java
+++ src/main/java/constant/InsuranceServiceConstants.java
@@ -11,11 +11,11 @@ public class InsuranceServiceConstants {
     public static final String ACCEPT_VALUE = "*/*";
     public static final String CLIENT_ID_WITH_POLICES = "4bdcabf8-ff9e-11ed-be56-0242ac120002";
     public static final String CLIENT_ID_NOT_EXIST = "6aa0d17a-74d0-45fd-8de8-7bd8a4d54f3f";
-    public static final String TYPE_OF_INSURANCE_CAR="1";
-    public static final String TYPE_OF_INSURANCE_HEALTH="2";
-    public static final String TYPE_OF_INSURANCE_ACCIDENT="5";
-    public static final String TYPE_OF_INSURANCE_TRAVELING="3";
-    public static final String TYPE_OF_INSURANCE_PROPERTY="4";
+    public static final String TYPE_OF_INSURANCE_CAR = "1";
+    public static final String TYPE_OF_INSURANCE_HEALTH = "2";
+    public static final String TYPE_OF_INSURANCE_ACCIDENT = "5";
+    public static final String TYPE_OF_INSURANCE_TRAVELING = "3";
+    public static final String TYPE_OF_INSURANCE_PROPERTY = "4";
     public static final String THING_INSURANCE_SERVICE_PRODUCT_NAME = "Страхование домашнего имущества";
     public static final String VEHICLE_OSAGO_INSURANCE_SERVICE_PRODUCT_NAME = "Автострахование ОСАГО";
     public static final String VEHICLE_KASKO_INSURANCE_SERVICE_PRODUCT_NAME = "Автострахование КАСКО";
@@ -24,9 +24,9 @@ public class InsuranceServiceConstants {
     public static final String INSURANCE_MEDICINE_STANDART_PLUS = "Добровольное медицинское страхование Standart+";
     public static final String INSURANCE_MEDICINE_PREMIUM = "Добровольное медицинское страхование Premium";
     public static final String INSURANCE_ACCIDENT = "Страхование от несчастных случаев";
-    public static final String INSURANCE_TRAVELING= "Страхование выезжающих за границу";
-    public static final String INSURANCE_FLAT= "Страхование квартиры";
-    public static final String INSURANCE_HOME= "Страхование дома";
+    public static final String INSURANCE_TRAVELING = "Страхование выезжающих за границу";
+    public static final String INSURANCE_FLAT = "Страхование квартиры";
+    public static final String INSURANCE_HOME = "Страхование дома";
     public static final String INVALID_POLICY_ID_ERROR_MESSAGE = "Некорректный ID полиса";
     public static final String FAKE_POLICY_ID_ERROR_MESSAGE = "Полис не найден";
 }
