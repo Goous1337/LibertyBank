1b37a5e61f2450dae85bb9ac5c1ae985ba8835a8
Dmitriy <idishinc@mail.ru>
2024-05-03 16:49:36 +0400

    Epic-1(125)

--- src/main/java/dataBase/requests/CustomerService_2_0_DataBaseRequest.java
+++ src/main/java/dataBase/requests/CustomerService_2_0_DataBaseRequest.java
@@ -27,6 +27,7 @@ public class CustomerService_2_0_DataBaseRequest {
         LOG.info(String.format("Получен код верификации: %s , по мобильному телефону: %s", verificationCode, mobilePhone));
         return verificationCode;
     }
+
     public static String getCustomerLastVerificationCodeById(String id) {
         String sql = "SELECT last_verification_code FROM user_profile WHERE customer_id =?::uuid";
         String verificationCode = getDBConnection(CUSTOMER_SERVICE_DB_2_0).queryForObject(sql, String.class, id);
