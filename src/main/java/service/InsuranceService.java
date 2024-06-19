128201889613cb45154b4bd4b98377fa365535bc
Hleb Zhuk <hleb7199@gmail.com>
2024-06-19 09:53:45 +0300

    checkstyle

--- src/main/java/service/InsuranceService.java
+++ src/main/java/service/InsuranceService.java
@@ -16,17 +16,14 @@ import static com.google.common.net.HttpHeaders.AUTHORIZATION;
 import static com.google.common.net.HttpHeaders.CONTENT_TYPE;
 import static constant.ApiEndpoints.APPLICATION_INSURANCE;
 import static constant.ApiEndpoints.APPLICATION_INSURANCE_OFFLINE;
-import static constant.ApiEndpoints.LIST_OF_INSURANCE;
 import static constant.ApiEndpoints.DEEP_OF_GROUPS;
 import static constant.ApiEndpoints.GROUPS_OF_POLICES;
-import static constant.ApiEndpoints.LIST_OF_INSURANCE_POLICES;
 import static constant.ApiEndpoints.POLICY_INSURANCE;
 import static constant.ApiEndpoints.POPULAR_INSURANCE_PRODUCTS;
 import static constant.InsuranceServiceConstants.ACCEPT_VALUE;
 import static constant.InsuranceServiceConstants.CONTENT_TYPE_VALUE;
 import static io.restassured.http.Method.GET;
 import static io.restassured.http.Method.POST;
-import static property.BaseProperties.ACCESS_TOKEN_INSURANCE_SERVICE;
 
 public class InsuranceService {
 
@@ -73,21 +70,6 @@ public class InsuranceService {
                 getRP(HEADER, AUTHORIZATION, token),
                 getRP(HEADER, CONTENT_TYPE, CONTENT_TYPE_VALUE));
         return sendSimpleRequest(GET, GROUPS_OF_POLICES + typeOfInsurance + DEEP_OF_GROUPS, params);
-    }
-
-    public Response checkGetListOfInsurancePolices(String clientId) {
-        List<RequestParam> params = List.of(getRP(HEADER, "accept", ACCEPT_VALUE),
-                getRP(HEADER, "clientId", clientId),
-                getRP(HEADER, AUTHORIZATION, ACCESS_TOKEN_INSURANCE_SERVICE),
-                getRP(HEADER, CONTENT_TYPE, CONTENT_TYPE_VALUE));
-        return sendSimpleRequest(GET, LIST_OF_INSURANCE_POLICES, params);
-    }
-
-    public Response checkGetListOfInsurancePolicesWithoutClientId() {
-        List<RequestParam> params = List.of(getRP(HEADER, "accept", ACCEPT_VALUE),
-                getRP(HEADER, AUTHORIZATION, ACCESS_TOKEN_INSURANCE_SERVICE),
-                getRP(HEADER, CONTENT_TYPE, CONTENT_TYPE_VALUE));
-        return sendSimpleRequest(GET, LIST_OF_INSURANCE_POLICES, params);
     }
 
     public Response makeNewApplicationInsuranceOffline(String clientId, OfflineInsuranceApplication offlineInsuranceApplication) {
