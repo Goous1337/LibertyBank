f889816ae7e0fd4ea7bfcbddf8a1cfe764d6e408
grausov.v <grausov.v@promail366.com>
2025-04-23 12:14:14 +0300

    add two assert verifications, pass respons bad, status code

--- src/test/java/api/insuranceService/INS_28_CheckApplyingForInsuranceOffline.java
+++ src/test/java/api/insuranceService/INS_28_CheckApplyingForInsuranceOffline.java
@@ -69,9 +69,7 @@ public class INS_28_CheckApplyingForInsuranceOffline extends BaseTest {
         Response response = insuranceService.makeNewApplicationInsuranceOfflineWithoutAuthorization(offlineInsuranceApplication);
         assertAll(
                 () -> assertEquals(SC_BAD_REQUEST, response.statusCode(), "Код ответа не соответствует ожидаемому"),
-                () -> assertEquals(NO_CLIENT_ID, response.jsonPath().get("message"), "Поле clientId является обязательным"),
-                () -> assertEquals(NO_CLIENT_ID,response.jsonPath().get("message"), "Поле clientId является обязательным" ),
-                () -> assertEquals(SC_BAD_REQUEST, response.statusCode(), "Код ответа соответвует ожидаемому")
+                () -> assertEquals(NO_CLIENT_ID, response.jsonPath().get("message"), "Поле clientId является обязательным")
         );
     }
 
